c49e38bf4f69374076dd163fb6a6f95fdd459bac
VietNga131 <161239556+VietNga131@users.noreply.github.com>
2026-04-05 23:45:56 +0700

    Update DongDuLieu.java

--- src/test/java/DongDuLieu.java
+++ src/test/java/DongDuLieu.java
@@ -1,3 +1,39 @@
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.example.Storm;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+@RunWith(Parameterized.class)
 public class DongDuLieu {
     
+    private int speed;
+    private int rainfall;
+    private boolean region;
+    private String eOutput;
+
+    public DongDuLieu(int speed, int rainfall, boolean region, String eOutput) {
+        this.speed = speed;
+        this.rainfall = rainfall;
+        this.region = region;
+        this.eOutput = eOutput;
+    }
+
+    @Parameterized.Parameters(name = "TC {index}: speed={0}, rainfall={1}, region={2} -> {3}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            { -1, 100, false, "Lỗi dữ liệu" }
+        });
+    }
+
+    @Test
+    public void testDongDieuKhien() {
+        String actualOutput = Storm.stormAssessment(speed, rainfall, region);
+        String errorMessage = String.format("Failed tại input: speed=%d, rainfall=%d, region=%b", speed, rainfall, region);
+
+        assertEquals(errorMessage, eOutput, actualOutput);
+    }
 }
