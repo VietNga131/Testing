e6ce6fd1d2f6d30ebd755d041498cb1cd2ebbcb9
VietNga131 <161239556+VietNga131@users.noreply.github.com>
2026-04-11 23:54:09 +0700

    Update DongDuLieu.java

--- src/test/java/DongDuLieu.java
+++ src/test/java/DongDuLieu.java
@@ -25,12 +25,20 @@ public class DongDuLieu {
     @Parameterized.Parameters(name = "TC {index}: speed={0}, rainfall={1}, region={2} -> {3}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-            { -1, 100, false, "Lỗi dữ liệu" }
+            { -1, 100, false, "Lỗi dữ liệu" },
+            { 160, 100, false, "Khẩn cấp - Yêu cầu sơ tán" },
+            { 100, 600, true, "Khẩn cấp - Yêu cầu sơ tán" },
+            { 50, 100, false, "Bình thường" },
+            { 80, 300, true, "Khẩn cấp - Yêu cầu sơ tán" },
+            { 80, 300, false, "Cảnh báo thời tiết xấu" },
+            { 50, 100, true, "Cảnh báo thời tiết xấu" },
+            { 80, 100, true, "Bão nguy hiểm" },
+            { 100, 100, false, "Cảnh báo thời tiết xấu" }
         });
     }
 
     @Test
-    public void testDongDieuKhien() {
+    public void testDongDuLieu() {
         String actualOutput = Stormm.stormAssessment(speed, rainfall, region);
         String errorMessage = String.format("Failed tại input: speed=%d, rainfall=%d, region=%b", speed, rainfall, region);
 
